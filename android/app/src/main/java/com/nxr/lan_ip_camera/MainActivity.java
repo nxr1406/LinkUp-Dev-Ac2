64b0eb38374d2f6356914399dd36ab071ac9c286
nxr1406 <chocqn2580@gmail.com>
2026-05-08 09:13:46 +0600

    update

---
+++ android/app/src/main/java/com/nxr/lan_ip_camera/MainActivity.java
@@ -0,0 +1,6 @@
+package com.nxr.lan_ip_camera;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/nxr/lan_ip_camera/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.nxr.lan_ip_camera
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
